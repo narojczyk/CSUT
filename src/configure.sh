75c29dfd0433625338a45eeeba27f19cce60db89
Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>
2021-11-26 00:45:26 +0100

    Basic initial configuration of master script.

--- src/configure.sh
+++ src/configure.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
+_RED=$(tput setaf 1)
+_RESET=$(tput sgr0)
 
-echo "[$0] This will configure CSUT on new location" |\
-  sed -e 's/\.sh]/]/' -e 's;\[\./;[;';
+conf_msg(){
+  echo "${_RED}[$0]${_RESET} $1" | sed -e 's/\.sh]/]/' -e 's;\[\./;[;'; }
 
+conf_msg "This will configure CSUT on new location"
 
-exit 0;
+EXIT_CODE=0;
+master="csut.sh"
+currentWorkDir=`pwd`
+parentDirName=`pwd | sed 's;^.*/;;'`
+
+if [ "${parentDirName}" = "src" ] && [ -f ${master} ]; then
+  eval "sed -i 's;CSUT\_CORE=.*;CSUT\_CORE=\"${currentWorkDir}\";' ${master}"
+  conf_msg "Finished"
+  exit ${EXIT_CODE};
+elif [ ! -f ${master} ]; then
+  conf_msg "Missing ${master}"
+  EXIT_CODE=1;
+elif [ "${parentDirName}" != "src" ]; then
+  conf_msg "Not running from 'src' directory. Check your location"
+  EXIT_CODE=1;
+fi
+
+conf_msg "${_RED}Configure failed.${_RESET}"
+exit ${EXIT_CODE};
