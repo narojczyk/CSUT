57f5d6cbd65cc0a837e9941b99bcd7a127636bd2
Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>
2022-11-29 21:21:24 +0100

    Add environment variables for the eagle cluster mode.

---
+++ src/modules/cluster/eagle/init/declare_environment_vars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define and declare additional environment variables for this script
+#   directories
+INITIALS="$CORE/init"
+
+#   utility programs
+#FPB="$UTILS/fancypb"    #TODO: check if exists
+#PYTHON="/usr/bin/python2"
+
+script_dirs=( INITIALS )
+
+
