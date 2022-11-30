f7a28c07a4cebcb9f95117cf19dbad0028fc0469
Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>
2022-11-30 14:56:16 +0100

    C binary has short buffor for the file name, the path has to be stripped. Remove link is now fixed.

--- src/modules/cluster/eagle/utils/calculate_average_exectime.sh
+++ src/modules/cluster/eagle/utils/calculate_average_exectime.sh
@@ -10,8 +10,8 @@ fi
 echo " Calculating an average execution time per job"
 cd $DEST;
 ln -s ${binavg} 2>/dev/null
-avmcs=`./average ${stepLog}`
-avtime=`./average ${timeLog}`
+avmcs=`./average ${stepLog##/*/}`
+avtime=`./average ${timeLog##/*/}`
 echo "Average calculation time: ${avtime} min."      > $averageJobStatsLog
 echo "Average MC steps: ${avmcs}"                   >> $averageJobStatsLog
 echo "Samples taken: `cat job_mc_steps.csv| wc -l`" >> $averageJobStatsLog
@@ -21,5 +21,5 @@ cat $averageJobStatsLog | sed 's/^/\t/'
 cd ${JOBSTARTER}
 
 # Clean  temporary files
-rm ${DESP}/average $stepLog $timelog
+rm ${DEST}/average $stepLog $timelog
 
