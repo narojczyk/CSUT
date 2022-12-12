8999cef477fb88689aec77edc5631e736b975dde
Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>
2022-12-12 10:40:31 +0100

    Fix path to utilities for Sijkl module, doe to recent changes.

--- src/modules/Sijkl/init/declare_environment_vars.sh
+++ src/modules/Sijkl/init/declare_environment_vars.sh
@@ -4,13 +4,14 @@
 #   directories
 MODULES="$CORE/modules"
 INITIALS="$CORE/init"
-UTILS="${CSUT_CORE_INC}/utils"
+CORE_UTILS="${CSUT_CORE_INC}/utils"
+UTILS="${CORE}/utils"
 GPLT="$CORE/gplt"
 HELPERS="$CORE/helper_scripts"
 SNPTDIR="$CODESDIR/SNpT"
 
 #   utility programs
-FPB="$UTILS/fancypb"    #TODO: check if exists
+FPB="${CORE_UTILS}/fancypb"
 PYTHON="/usr/bin/python2"
 
 script_dirs=( MODULES INITIALS UTILS GPLT HELPERS SNPTDIR )
