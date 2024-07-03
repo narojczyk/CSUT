bf70b3295a100fd29cd5a20853e6c54e6bbb05db
Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>
2024-07-03 19:37:12 +0200

    Switch from 'if' to 'case' to simplify opMode setting. Fix obsolete log message when opMode value is not passed.

--- src/modules/Sijkl/init/apply_argv.sh
+++ src/modules/Sijkl/init/apply_argv.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 # Based on mode setting prepare the name of the work directory
-if [ "$opMode" = "dev" ]; then
-  hereANDnow=`date +"%F"`
-  debugMode=1
-elif [ "$opMode" = "aux" ]; then
-  SCRATCH=$SCRATCH1
-else
-    comment="Current value for opMode=$opMode has no effect"
-    if [ -z ${logFile:+x} ]; then 
-        echo $comment 
-    else
-        echo $comment >> $logFile
+case "$opMode" in
+    "dev" )
+      hereANDnow=`date +"%F"`
+      debugMode=1
+      ;;
+
+    "aux" )
+      SCRATCH=$SCRATCH1
+      ;;
+
+    *)
+    if [ ${#opMode} -gt 0 ]; then
+      comment="[`date +"%F_%H-%M-%S"`] ($SNAME unit `echo $(basename $BASH_SOURCE)`) Current value for opMode=$opMode has no effect"
+      if [ -z ${logFile:+x} ]; then
+          echo $comment
+      else
+          echo $comment >> $logFile
+      fi
     fi
-fi
+      ;;
+esac
 
 if [ $plotWithoutExcluded -ne 0 ]; then
   if [ -z ${excludeIDsFile:+x} ] || \
